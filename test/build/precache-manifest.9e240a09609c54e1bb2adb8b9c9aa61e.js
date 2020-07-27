cbca46a3d103ed70ed11167f291befb1f6891942
Margarita Matulenko <margarita.matulenko@cognziant.com>
2020-07-27 10:53:31 +0300

    lecture 46

---
+++ test/build/precache-manifest.9e240a09609c54e1bb2adb8b9c9aa61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87308cd13b0c48746609e4d621d45d10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ab4078aaf01380a9277",
+    "url": "/static/css/main.963323ca.chunk.css"
+  },
+  {
+    "revision": "2207913c5a3fbf86ada7",
+    "url": "/static/js/2.fab44c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fab44c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab4078aaf01380a9277",
+    "url": "/static/js/main.f2fe5887.chunk.js"
+  },
+  {
+    "revision": "55dc2d6d8891bead9b41",
+    "url": "/static/js/runtime-main.31687faf.js"
+  }
+]);
